97cdf1f9bf2593adbf73cd7116d6e8f67b17507a
dlwas <57624006+dlwas@users.noreply.github.com>
2021-10-01 15:09:14 +0100

    Create GameQuestion.vue

---
+++ src/components/GameQuestion.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-72">
+    <h3 class="uppercase text-xl font-bold">
+      {{ question }}
+    </h3>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  question: {
+    type: String,
+    default: '',
+  },
+})
+</script>
