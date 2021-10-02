6d8f29c198fb0a2aa58a64ac462d1299ad623e71
dlwas <57624006+dlwas@users.noreply.github.com>
2021-10-02 15:12:05 +0100

    Create share.vue

---
+++ src/views/share.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>share</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'share',
+  components: {},
+  props: {},
+  setup() {
+    return {}
+  },
+})
+</script>
