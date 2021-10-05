43f753d9d1e1f938b489dd37b57359cdcd3ff0e6
dlwas <57624006+dlwas@users.noreply.github.com>
2021-10-05 23:27:54 +0200

    Update about.vue

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,16 +1,36 @@
 <template>
-  <div>about</div>
+  <div class="w-full text-center">
+    <OptionGroup class="mb-6" text="Contact">
+      <Button class="w-min">
+        <p class="normal-case">darek.leszek.wasilewski@gmail.com</p>
+      </Button>
+    </OptionGroup>
+    <OptionGroup class="mb-6" text="Repository">
+      <Button class="w-min">
+        <a class="normal-case" href="https://github.com/dlwas/quiz">
+          https://github.com/dlwas/quiz
+        </a>
+      </Button>
+    </OptionGroup>
+    <OptionGroup class="mb-6" text="Github">
+      <Button class="w-min">
+        <a class="normal-case" href="https://github.com/dlwas">https://github.com/dlwas</a>
+      </Button>
+    </OptionGroup>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Label from '../components/Label.vue'
+import Button from '../components/Button.vue'
+import OptionGroup from '../components/OptionGroup.vue'
 
 export default defineComponent({
   name: 'about',
-  components: {},
+  components: { Label, Button, OptionGroup },
   props: {},
   setup() {
-
     return {}
   },
 })
